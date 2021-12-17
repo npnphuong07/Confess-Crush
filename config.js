6cb4de7d2b74aae5fa7289c81eefd9d3fef11fb3
phamkhanhnhi <96271904+phamkhanhnhi@users.noreply.github.com>
2021-12-17 08:34:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
     titleWeb: "Gửi bạn dễ thương",
-    introTitle: 'Bạn cute',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Bạn gì đó cute oii',
+    introDesc: `Bạn oiii
+    Đông đến cận kề  
+    Bao nhiêu lớp áo
+    Không bằng love tớ  `,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    title: 'Bạn cho mình làm quen được hông 🥰',
+    desc: 'Chịu honggg ',
+    btnYes: 'Okie nè <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    question: 'Bạn đồng ý gòi đó nhaaaa <3',
+    btnReply: 'Tặng bạn tym nek <3',
+    reply: 'Chịu được tới đây là bạn đồng ý làm quen gòi',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'Ib mình đi mòaaa',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/messages/t/100075288410109' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
